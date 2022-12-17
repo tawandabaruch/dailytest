46d071dea493b879a2c4af8869ab8269794bd2d6
thankmoretaderera <ishmaelpastor@gmail.com>
2022-12-17 11:59:18 +0200

    test

---
+++ src/pages/meeting.js
@@ -0,0 +1,37 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+export default function Video({ match }) {
+    const id = match.params.id;
+
+    useEffect(() => {
+        const domain = "";
+
+        axios
+            .get(`/video-call/${id}`)
+            .then((res) => {
+                if (res.status === 200) {
+                    const script = document.createElement("script");
+                    script.innerHTML = `window.DailyIframe.createFrame({
+            iframeStyle: {
+              position: "relative",
+              width: "100%",
+              height: "100%",
+              border: "0",
+              zIndex: 9999
+            },
+            showLeaveButton: true,
+            showFullscreenButton: true,
+          }).join({
+            url: "${domain}${id}",
+          });`;
+
+                    document.body.appendChild(script);
+                }
+            })
+            .catch((err) => console.log(err));
+    }, [id]);
+
+    return <div></div>;
+}
+
